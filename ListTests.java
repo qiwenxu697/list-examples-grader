e6ae894f50e6213d37f091e7e19210455a1b112b
qiwenxu697 <qiwen.x67@gmail.com>
2024-03-07 10:32:36 -0800

    grader save

---
+++ ListTests.java
@@ -0,0 +1,25 @@
+
+import static org.junit.Assert.*;
+
+import java.util.*;
+
+import org.junit.*;
+
+public class ListTests {
+    public boolean checkString(String s){
+
+    }
+    @Test 
+    public void testFilter() {
+        List<String> l1 = new ArrayList<String>(); 
+        l1.add("apple");
+        l1.add("banana");
+        l1.add("hi");
+        
+    }
+
+	@Test 
+	public void testListMerge() {
+        
+	}
+}
--- grade.sh
+++ grade.sh
@@ -3,10 +3,33 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 rm -rf student-submission
 rm -rf grading-area
 
+mkdir student-submission
 mkdir grading-area
 
 git clone $1 student-submission
 echo 'Finished cloning'
+
+if ! [ -f student-submission/ListExamples.java ]
+then 
+    echo "Missing Necessary File"
+    exit 1
+fi
+pwd
+echo "continue"
+
+cp TestListExamples.java student-submission/ListExamples.java grading-area
+cp -r lib grading-area
+
+cd grading-area
+pwd
+
+javac -cp $CPATH *.java
+if [ $? -ne 0 ]
+then 
+    echo 'compiler error'
+    exit 1
+fi
+
 
 
 # Draw a picture/take notes on the directory structure that's set up after
